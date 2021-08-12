9f9d35e0d6364db0c0a0ebee40681824e4319a8c
Ross Viljoen <ross@viljoen.co.uk>
2021-08-13 00:13:51 +0100

    Update classification example

--- examples/classification.jl
+++ examples/classification.jl
@@ -1,15 +1,16 @@
-# Recreation of https://gpflow.readthedocs.io/en/master/notebooks/basics/classification.html
+# (Roughly) a recreation of https://gpflow.readthedocs.io/en/master/notebooks/basics/classification.html
 
-# %%
+# # One-Dimensional Stochastic Variational Classification
+#
+# ## Setup
+
 using SparseGPs
 using AbstractGPs
 using GPLikelihoods
-using StatsFuns
-using FastGaussQuadrature
 using Distributions
 using LinearAlgebra
-using DelimitedFiles
 using IterTools
+using Flux
 
 using Plots
 default(; legend=:outertopright, size=(700, 400))
@@ -17,87 +18,115 @@ default(; legend=:outertopright, size=(700, 400))
 using Random
 Random.seed!(1234)
 
-# %%
-# Read in the classification data
-data_file = pkgdir(SparseGPs) * "/examples/data/classif_1D.csv"
-x, y = eachcol(readdlm(data_file))
-scatter(x, y)
+# ## Sampling some data
+#
+# In this example, we shall see whether the sparse variational Gaussian process
+# (SVGP) can recover the true GP from which binary classification data are
+# sampled.
 
-# %%
-# First, create the GP kernel from given parameters k
+# First, a helper function to create the GP kernel
+
 function make_kernel(k)
     return softplus(k[1]) * (Matern52Kernel() ∘ ScaleTransform(softplus(k[2])))
 end
+kernel_params = [10.0, 0.5] # The true kernel parameters
+#md nothing #hide
 
-k = [20.0, 0.5]
+# Create the 'ground truth GP' and sample a function
+
+k = make_kernel(kernel_params)
+f = LatentGP(GP(k), BernoulliLikelihood(), 1e-6)
+x_true = 0:0.02:6
+f_true, y_true = rand(f(x_true))
+#md nothing #hide
+
+# Plot the sampled function
+plot(x_true, f_true; seriescolor="red", label="")
+
+# Plot the function pushed through a logistic sigmoid, restricting it to ``[0, 1]``
+plot(x_true, mean.(f.lik.(f_true)); seriescolor="red", label="")
+
+# Subsample input locations to obtain training data
+
+N_train = 30
+mask = sample(1:length(x_true), N_train; replace=false, ordered=true)
+x, _, y = x_true[mask], f_true[mask], y_true[mask]
+scatter(x, y; label="Sampled data")
+plot!(x_true, mean.(f.lik.(f_true)); seriescolor="red", label="True function")
+
+# ## Setting up the SVGP model
+
 M = 15 # number of inducing points
-z = x[1:M]
+z = sample(x, M; replace=false)
 
-model = SVGPModel(make_kernel, k, z; jitter=1e-3, likelihood=BernoulliLikelihood())
+model = SVGPModel(
+    make_kernel,    # The function to create the kernel
+    rand(2),        # The initial kernel parameters
+    z;              # The inducing inputs
+    jitter=1e-4,
+    likelihood=BernoulliLikelihood()
+)
+#md nothing #hide
+
+# Plot some samples from the prior of this model
 
 f = prior(model)
 fx = f(x)
-
-# %%
-# Then, plot some samples from the prior underlying GP
 x_plot = 0:0.02:6
-prior_f_samples = rand(f.f(x_plot), 20) # TODO: add jitter?
+prior_f_samples = rand(f.f(x_plot), 20)
 
-plt = plot(x_plot, prior_f_samples; seriescolor="red", linealpha=0.2, label="")
-scatter!(plt, x, y; seriescolor="blue", label="Data points")
+plot(x_plot, prior_f_samples; seriescolor="red", linealpha=0.2, label="")
 
-# %%
-# Plot the same samples, but pushed through a logistic sigmoid to constrain
-# them in (0, 1).
+# Plot the same samples, but pushed through a logistic sigmoid as before
+
 prior_y_samples = mean.(f.lik.(prior_f_samples))
 
-plt = plot(x_plot, prior_y_samples; seriescolor="red", linealpha=0.2, label="")
-scatter!(plt, x, y; seriescolor="blue", label="Data points")
+plot(x_plot, prior_y_samples; seriescolor="red", linealpha=0.2, label="")
+scatter!(x, y; seriescolor="blue", label="Data points")
 
-# %%
-# Optimise the model using Flux
-using Flux
+# ## Train the model using Flux
 
 opt = ADAM(0.1)
 parameters = Flux.params(model)
 delete!(parameters, model.z)    # Don't train the inducing inputs
+#md nothing #hide
 
-# %%
 # Negative ELBO before training
+
 println(loss(model, x, y))
 
-# %%
-# Train the model
+# The main training loop
+
 Flux.train!(
     (x, y) -> loss(model, x, y),
     parameters,
-    ncycle([(x, y)], 2000), # Train for 1000 epochs
+    ncycle([(x, y)], 6000), # Train for 6000 epochs
     opt,
 )
+#md nothing #hide
 
-# %%
 # Negative ELBO after training
+
 println(loss(model, x, y))
 
-# %%
 # After optimisation, plot samples from the underlying posterior GP.
+
 post = posterior(model)
+post_f_samples = rand(post.f(x_plot), 20)
 
-post_f_samples = rand(post.f(x_plot), 20) # TODO: add jitter?
+plot(x_plot, post_f_samples; seriescolor="red", linealpha=0.2, legend=false)
 
-plt = plot(x_plot, post_f_samples; seriescolor="red", linealpha=0.2, legend=false)
+# As above, push these samples through a logistic sigmoid to get posterior samples.
 
-# %%
-# As above, push these samples through a logistic sigmoid to get posterior predictions.
 post_y_samples = mean.(post.lik.(post_f_samples))
 
-plt = plot(
+plot(
     x_plot,
     post_y_samples;
-    seriescolor="red",
+    seriescolor="green",
     linealpha=0.2,
-    # legend=false,
     label="",
 )
-scatter!(plt, x, y; seriescolor="blue", label="Data points")
+scatter!(x, y; seriescolor="blue", label="Data points")
 vline!(z; label="Pseudo-points")
+plot!(x_true, mean.(f.lik.(f_true)); seriescolor="red", linewidth=3, label="True function")
