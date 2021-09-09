7bb1214d6942cad96b86e6b20b4dd375b48a63df
ST John <st--@users.noreply.github.com>
2021-09-09 15:30:12 +0300

    use local implementation of KL

--- src/ApproximateGPs.jl
+++ src/ApproximateGPs.jl
@@ -6,18 +6,17 @@ using Reexport
 using Distributions
 using LinearAlgebra
 using Statistics
-using StatsBase
 using FastGaussQuadrature
 using SpecialFunctions
 using ChainRulesCore
 using FillArrays
-using KLDivergences
 
 using AbstractGPs: AbstractGP, FiniteGP, LatentFiniteGP, ApproxPosteriorGP, At_A, diag_At_A
 
 export SVGP, DefaultQuadrature, Analytic, GaussHermite, MonteCarlo
 
 include("utils.jl")
+include("kldiv.jl")
 include("svgp.jl")
 include("elbo.jl")
 
---
+++ src/kldiv.jl
@@ -0,0 +1,17 @@
+function KL(p::AbstractMvNormal, q::AbstractMvNormal)
+    # This is the generic implementation for AbstractMvNormal, you might need to specialize for your type
+    length(p) == length(q) || 
+        throw(DimensionMismatch("Distributions p and q have different dimensions $(length(p)) and $(length(q))"))
+    Σp, Σq = cov.((p, q))
+    Δμ = mean(p) - mean(q)
+    0.5 * (tr(Σq \ Σp) + dot(Δμ, Σq \ Δμ) - length(p) + logdet(Σq) - logdet(Σp))
+end
+
+function KL(p::MvNormal, q::MvNormal)
+    # We use p.Σ and q.Σ to take the advantage that they are defined as PDMats objects
+    length(p) == length(q) || 
+        throw(DimensionMismatch("Distributions p and q have different dimensions $(length(p)) and $(length(q))"))
+    0.5 * (tr(q.Σ \ p.Σ) + invquad(q.Σ, mean(p) - mean(q)) - length(p) + logdet(q.Σ) - logdet(p.Σ))
+end
+
+kldivergence(p, q) = KL(p, q)
