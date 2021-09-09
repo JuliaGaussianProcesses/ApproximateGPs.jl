76fd45bec68375c0a4e1aa0b3ac19ccbd6fddeb0
ST John <st--@users.noreply.github.com>
2021-09-09 15:37:28 +0300

    bugfixes

--- src/ApproximateGPs.jl
+++ src/ApproximateGPs.jl
@@ -6,10 +6,12 @@ using Reexport
 using Distributions
 using LinearAlgebra
 using Statistics
+using StatsBase
 using FastGaussQuadrature
 using SpecialFunctions
 using ChainRulesCore
 using FillArrays
+using PDMats
 
 using AbstractGPs: AbstractGP, FiniteGP, LatentFiniteGP, ApproxPosteriorGP, At_A, diag_At_A
 
