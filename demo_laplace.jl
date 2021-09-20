4853b5fb087ed38d27d2f5b531e4cb8c171dfd4f
ST John <st--@users.noreply.github.com>
2021-09-20 20:42:37 +0300

    *Very* WIP - but works

--- demo_laplace.jl
+++ demo_laplace.jl
@@ -23,6 +23,20 @@ end
 
 dist_y_given_f(f) = Bernoulli(invlink(f))
 
+function build_latent_gp(theta)
+    variance = softplus(theta[1])
+    lengthscale = softplus(theta[2])
+    kernel = variance * with_lengthscale(SqExponentialKernel(), lengthscale)
+    return LatentGP(GP(kernel), dist_y_given_f, 1e-8)
+end
+
+function plot_samples!(Xgrid, fpost; samples=100, color=2)
+    fsamples = rand(fpost(Xgrid, 1e-8), samples)
+    plot!(Xgrid, invlink.(fsamples); color, alpha=0.3, label="")
+end
+
+using Zygote
+
 theta0 = [0.0, 1.0]
 
 optimizer = LBFGS(;
@@ -31,9 +45,39 @@ optimizer = LBFGS(;
 )
 optim_options = Optim.Options(; iterations=100)
 
-function build_latent_gp(theta)
-    variance = softplus(theta[1])
-    lengthscale = softplus(theta[2])
-    kernel = variance * with_lengthscale(SqExponentialKernel(), lengthscale)
-    return LatentGP(GP(kernel), dist_y_given_f, 1e-8)
+lf = build_latent_gp(theta0)
+lfX = lf(X)
+
+newt_res = laplace_steps(lfX.lik, lfX.fx, Y)
+
+f_post, opt_res = ApproximateGPs.optimize_elbo(build_latent_gp, theta0, X, Y, NelderMead(), optim_options)
+
+theta1 = opt_res.minimizer
+
+function full_objective(theta)
+    Zygote.ignore() do
+        # Zygote does not like the try/catch within @info
+        @info "Hyperparameters: $theta"
+    end
+    lf = build_latent_gp(theta)
+    f = zeros(length(X))
+    lml = ApproximateGPs.laplace_lml!(f, lf(X), Y)
+    return -lml
+end
+
+function nonewton_objective(theta)
+    _lf = build_latent_gp(theta)
+    return -ApproximateGPs.laplace_lml_nonewton(newt_res[end].f, _lf(X), Y)
+end
+
+using FiniteDifferences
+
+FiniteDifferences.grad(central_fdm(5, 1), full_objective, theta0)
+
+
+function comp_lml(theta)
+    _lf = build_latent_gp(theta)
+    K = kernelmatrix(_lf.f.kernel, X)
+    dist_y_given_f = _lf.lik
+    return ApproximateGPs.laplace_lml(K, dist_y_given_f, Y; maxiter=100)
 end
--- src/laplace.jl
+++ src/laplace.jl
@@ -125,57 +125,144 @@ function laplace_lml_nonewton(f_opt, lfX, Y)
     # d lml / d theta = \partial lml / \partial theta + \partial lml / \partial fhat \partial fhat / \partial theta
 end
 
-function newton_inner_loop(K, dist_y_given_f, ys; f_init, maxiter)
+NEWTON_COUNTER = 0
+
+function _reset()
+	global NEWTON_COUNTER
+	NEWTON_COUNTER = 0
+end
+
+function _newton_inner_loop(K, dist_y_given_f, ys; f_init, maxiter)
+    global NEWTON_COUNTER
     f = f_init
+    cache = nothing
     for i in 1:maxiter
-        @info "  - Newton iteration $i"
+	Zygote.ignore() do
+		@info "  - Newton iteration $i: f[1:3]=$(f[1:3])"
+	end
         fnew, cache = _newton_step(dist_y_given_f, ys, K, f)
+	NEWTON_COUNTER += 1
 
         if isapprox(f, fnew)
-            @info "  + converged"
+	    Zygote.ignore() do
+		    @info "  + converged"
+	    end
             break  # converged
         else
             f = fnew
         end
     end
-    return fnew #, cache
+    return f, cache
 end
 
-function ChainRulesCore.rrule(
-    config::RuleConfig{>:HasReverseMode},
-    ::typeof(newton_inner_loop),
-    K,
-    dist_y_given_f,
-    Y;
-    f_init,
-    maxiter,
-)
-    fopt = newton_inner_loop(K, dist_y_given_f, Y; f_init, maxiter)
-    cache = _laplace_train_intermediates(dist_y_given_f, Y, K, fopt)
+function newton_inner_loop(K, dist_y_given_f, ys; f_init, maxiter)
+    f_opt, _ = _newton_inner_loop(K, dist_y_given_f, ys; f_init, maxiter)
+    return f_opt
+end
 
-    function newton_pullback(Δfopt)
-        δself = NoTangent()
-        function newton_single_step(K, dist_y_given_f, ys)
-            fnew, cache = _newton_step(dist_y_given_f, ys, K, fopt)
-            return fnew
-        end
+function ChainRulesCore.frule((Δself, ΔK, Δdist_y_given_f, Δys), ::typeof(newton_inner_loop), K, dist_y_given_f, ys; f_init, maxiter)
+    f_opt, cache = _newton_inner_loop(K, dist_y_given_f, ys; f_init, maxiter)
 
-        fopt2, d_fopt = rrule_via_ad(config, newton_single_step, K, dist_y_given_f, Y)
-        return d_fopt(Δfopt)
-        dfopt_dK_times_Δfopt = d_fopt(Δfopt)[2]
+    # f = K grad_log_p_y_given_f(f)
+    # fdot = Kdot grad_log_p_y_given_f(f) + K grad2_log_p_y_given_f(f) fdot
+    # fdot (I - K grad2_log_p_y_given_f(f)) = Kdot grad_log_p_y_given_f(f)
+    # fdot = (I - K grad2_log_p_y_given_f(f))⁻¹ Kdot grad_log_p_y_given_f(f)
+    # (I - K grad2_log_p_y_given_f(f)) = (I + K W) = (√W)⁻¹ (I + √W K √W) √W = (√W)⁻¹ B √W
+    # fdot = (√W)⁻¹ B⁻¹ √W Kdot grad_log_p_y_given_f(f)
+    ∂f_opt = cache.Wsqrt \ (cache.B_ch \ (cache.Wsqrt * (ΔK * cache.d_loglik)))
+
+    @info "Hit frule"
+
+    return f_opt, (∂f_opt, NoTangent())
+end
+
+function ChainRulesCore.rrule(::typeof(newton_inner_loop), K, dist_y_given_f, ys; f_init, maxiter)
+    @info "Hit rrule"
+    f_opt, cache = _newton_inner_loop(K, dist_y_given_f, ys; f_init, maxiter)
+
+    # f = K grad_log_p_y_given_f(f)
+    # fdot = Kdot grad_log_p_y_given_f(f) + K grad2_log_p_y_given_f(f) fdot
+    # fdot (I - K grad2_log_p_y_given_f(f)) = Kdot grad_log_p_y_given_f(f)
+    # fdot = (I - K grad2_log_p_y_given_f(f))⁻¹ Kdot grad_log_p_y_given_f(f)
+    # (I - K grad2_log_p_y_given_f(f)) = (I + K W) = (√W)⁻¹ (I + √W K √W) √W = (√W)⁻¹ B √W
+    # fdot = (√W)⁻¹ B⁻¹ √W Kdot grad_log_p_y_given_f(f)
+
+    # ∂f_opt = cache.Wsqrt \ (cache.B_ch \ (cache.Wsqrt * (ΔK * cache.d_loglik)))
+
+    # Re<Δf, fdot> = Re<Δf, Wsqrt\inv B\inv Wsqrt Kdot d_loglik>
+    #              = Re<Wsqrt' Binv' Wsqrt\inv' Δf d_loglik', Kdot >
+    #
+    # ΔK = Wsqrt * cache.B_ch' \ Wsqrt \ Δf_opt * cache.d_loglik'
+
+    function newton_pullback(Δf_opt)
+        ∂self = NoTangent()
+
+        # ΔK = cache.B_ch' \ Δf_opt * cache.d_loglik'
+	dfopt_dK_times_Δfopt = @thunk(cache.Wsqrt * (cache.B_ch \ (cache.Wsqrt \ Δf_opt)) * cache.d_loglik')
 
         dfopt_dlik_times_Δfopt = @not_implemented(
             "gradient of lml w.r.t. likelihood parameters"
         )
         dfopt_dY_times_Δfopt = @not_implemented("gradient of lml w.r.t. observations")
-        return (δself, dfopt_dK_times_Δfopt, dfopt_dlik_times_Δfopt, dfopt_dY_times_Δfopt)
+        return (∂self, dfopt_dK_times_Δfopt, dfopt_dlik_times_Δfopt, dfopt_dY_times_Δfopt)
     end
 
-    return (fnew, cache), newton_pullback
+    return f_opt, newton_pullback
 end
 
-function laplace_lml(K, dist_y_given_f, Y; f=zeros(length(Y)), maxiter)
-    f_opt, cache = newton_inner_loop(dist_y_given_f, Y, K; f, maxiter)
+#function _newton_inner_loop_dual(::Type{T}, dual_args...) where {T<:Dual}
+#    ȧrgs = (NO_FIELDS,  partials.(dual_args)...)
+#    args = (_newton_inner_loop, value.(dual_args)...)
+#    y, ẏ = frule(ȧrgs, args...)
+#    T(y, ẏ)
+#end
+#
+#function __newton_inner_loop(ϵ1::T1, ϵ2::T2, θ::T3) where {T1,T2,T3}
+#    T = promote_type(T1, T2, T3)
+#    if T <: Dual
+#        _solve_dual(T, ϵ1, ϵ2, θ)
+#    else
+#        _solve(ϵ1, ϵ2, θ)
+#    end
+#end
+
+
+#function ChainRulesCore.rrule(
+#    config::RuleConfig{>:HasReverseMode},
+#    ::typeof(newton_inner_loop),
+#    K,
+#    dist_y_given_f,
+#    Y;
+#    f_init,
+#    maxiter,
+#)
+#    fopt = newton_inner_loop(K, dist_y_given_f, Y; f_init, maxiter)
+#    cache = _laplace_train_intermediates(dist_y_given_f, Y, K, fopt)
+#
+#    function newton_pullback(Δfopt)
+#        δself = NoTangent()
+#        function newton_single_step(K, dist_y_given_f, ys)
+#            fnew, cache = _newton_step(dist_y_given_f, ys, K, fopt)
+#            return fnew
+#        end
+#
+#        fopt2, d_fopt = rrule_via_ad(config, newton_single_step, K, dist_y_given_f, Y)
+#        return d_fopt(Δfopt)
+#        dfopt_dK_times_Δfopt = d_fopt(Δfopt)[2]
+#
+#        dfopt_dlik_times_Δfopt = @not_implemented(
+#            "gradient of lml w.r.t. likelihood parameters"
+#        )
+#        dfopt_dY_times_Δfopt = @not_implemented("gradient of lml w.r.t. observations")
+#        return (δself, dfopt_dK_times_Δfopt, dfopt_dlik_times_Δfopt, dfopt_dY_times_Δfopt)
+#    end
+#
+#    return (fnew, cache), newton_pullback
+#end
+
+function laplace_lml(K, dist_y_given_f, Y; f_init=zeros(length(Y)), maxiter)
+    f_opt = newton_inner_loop(K, dist_y_given_f, Y; f_init, maxiter)
+    cache = _laplace_train_intermediates(dist_y_given_f, Y, K, f_opt)
     return _laplace_lml(f_opt, cache)
 end
 
@@ -195,7 +282,7 @@ end
 #    return lml, laplace_lml_pullback
 #end
 
-function optimize_elbo(build_latent_gp, theta0, X, Y, optimizer, optim_options)
+function optimize_elbo(build_latent_gp, theta0, X, Y, optimizer, optim_options, newton_warmstart=true)
     lf = build_latent_gp(theta0)
     f = mean(lf(X).fx)  # will be mutated in-place to "warm-start" the Newton steps
 
@@ -205,7 +292,18 @@ function optimize_elbo(build_latent_gp, theta0, X, Y, optimizer, optim_options)
             @info "Hyperparameters: $theta"
         end
         lf = build_latent_gp(theta)
-        lml = laplace_lml!(f, lf(X), Y)
+        #lml = laplace_lml!(f, lf(X), Y)
+	lfX = lf(X)
+	K = cov(lfX.fx)
+	dist_y_given_f = lfX.lik
+        f_opt = newton_inner_loop(K, dist_y_given_f, Y; f_init=f, maxiter=100)
+	cache = _laplace_train_intermediates(dist_y_given_f, Y, K, f_opt)
+        lml = _laplace_lml(f_opt, cache)
+	Zygote.ignore() do
+		if newton_warmstart
+			f .= f_opt
+		end
+	end
         return -lml
     end
 
