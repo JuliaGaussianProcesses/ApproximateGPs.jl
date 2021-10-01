bf98f1604eea88da3fbc80ea47187d29152b6d97
ST John <st--@users.noreply.github.com>
2021-10-01 11:35:39 +0300

    run all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,23 +22,23 @@ const PKGDIR = dirname(dirname(pathof(ApproximateGPs)))
 include("test_utils.jl")
 
 @testset "ApproximateGPs" begin
-    #include("expected_loglik.jl")
-    #println(" ")
-    #@info "Ran expected_loglik tests"
-    #
-    #@testset "SVGP" begin
-    #    include("svgp.jl")
-    #    println(" ")
-    #    @info "Ran svgp tests"
-    #
-    #    include("elbo.jl")
-    #    println(" ")
-    #    @info "Ran elbo tests"
-    #
-    #    include("equivalences.jl")
-    #    println(" ")
-    #    @info "Ran equivalences tests"
-    #end
+    include("expected_loglik.jl")
+    println(" ")
+    @info "Ran expected_loglik tests"
+    
+    @testset "SVGP" begin
+        include("svgp.jl")
+        println(" ")
+        @info "Ran svgp tests"
+    
+        include("elbo.jl")
+        println(" ")
+        @info "Ran elbo tests"
+    
+        include("equivalences.jl")
+        println(" ")
+        @info "Ran equivalences tests"
+    end
 
     include("common_test_support.jl")
 
