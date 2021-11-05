b0e69d2ecb3aa01a58d4d9aae197ca01ac523994
WT <wt0881@my.bristol.ac.uk>
2021-11-05 16:54:46 +0000

    Improve docs

--- src/whitened_sparse_variational.jl
+++ src/whitened_sparse_variational.jl
@@ -13,6 +13,21 @@ end
 
 raw"""
     posterior(sva::WhitenedSparseVariationalApproximation)
+
+Compute the approximate posterior [1] over the process `f =
+sva.fz.f`, given inducing inputs `z = sva.fz.x` and a variational
+distribution over inducing points `sva.q_ε` (which represents ``q(ε)``
+where `ε = cholesky(cov(fz)).U' * (f(z) - mean(f(z)))`). The approximate posterior at test
+points ``x^*`` where ``f^* = f(x^*)`` is then given by:
+
+```math
+q(f^*) = \int p(f | ε) q(ε) du
+```
+which can be found in closed form.
+
+[1] - Hensman, James, Alexander Matthews, and Zoubin Ghahramani. "Scalable
+variational Gaussian process classification." Artificial Intelligence and
+Statistics. PMLR, 2015.
 """
 function AbstractGPs.posterior(approx::WhitenedSparseVariationalApproximation)
     fz = approx.fz
