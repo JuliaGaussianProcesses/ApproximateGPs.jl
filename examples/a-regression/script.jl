3db8395d6cc47ab21dfb908df4559d49dcefe2fb
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2022-01-20 19:34:55 +0100

    Fix format

--- examples/a-regression/script.jl
+++ examples/a-regression/script.jl
@@ -30,7 +30,7 @@ N = 10000 # Number of training points
 x = rand(Uniform(-1, 1), N)
 y = g.(x) + 0.3 * randn(N)
 
-scatter(x, y; xlabel="x", ylabel="y", markershape=:xcross, markeralpha=0.1, legend=false) |> DisplayAs.PNG
+DisplayAs.PNG(scatter(x, y; xlabel="x", ylabel="y", markershape=:xcross, markeralpha=0.1, legend=false))
 
 # ## Set up a Flux model
 #
@@ -155,7 +155,7 @@ scatter(
     markeralpha=0.1,
     label="Training Data",
 )
-plot!(-1:0.001:1, init_post; label="Initial Posterior", color=4) |> DisplayAs.PNG
+DisplayAs.PNG(plot!(-1:0.001:1, init_post; label="Initial Posterior", color=4))
 
 # ## Training the model
 #
@@ -209,4 +209,4 @@ scatter(
     color=1,
 )
 plot!(-1:0.001:1, post; label="Posterior", color=4)
-sticks!(model.z, fill(0.13, M); label="Pseudo-points", linewidth=1.5, color=5) |> DisplayAs.PNG
+DisplayAs.PNG(sticks!(model.z, fill(0.13, M); label="Pseudo-points", linewidth=1.5, color=5))
