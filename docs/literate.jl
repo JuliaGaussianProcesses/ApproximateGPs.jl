56cd99958fdeb26a60363b33aa341e490e46f7bb
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-01-20 18:13:23 +0100

    Update docs/literate.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/literate.jl
+++ docs/literate.jl
@@ -12,7 +12,7 @@ const EXAMPLEPATH = joinpath(@__DIR__, "..", "examples", EXAMPLE)
 Pkg.activate(EXAMPLEPATH)
 Pkg.instantiate()
 pkg_status = sprint() do io
-    Pkg.status(;io=io)
+    Pkg.status(; io=io)
 end
 manifest_status = sprint() do io
     Pkg.status(;io=io, mode=Pkg.PKGMODE_MANIFEST)
