84a61377dec8728d831bf2f6ec293e7dd7b1e6cf
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-01-28 17:17:21 +0100

    Update examples/a-regression/script.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- examples/a-regression/script.jl
+++ examples/a-regression/script.jl
@@ -30,10 +30,7 @@ N = 10000 # Number of training points
 x = rand(Uniform(-1, 1), N)
 y = g.(x) + 0.3 * randn(N)
 
-
-scatter(
-    x, y; xlabel="x", ylabel="y", markershape=:xcross, markeralpha=0.1, legend=false
-)
+scatter(x, y; xlabel="x", ylabel="y", markershape=:xcross, markeralpha=0.1, legend=false)
 DisplayAs.PNG(ans) # hide
 
 # ## Set up a Flux model
