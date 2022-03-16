69ef2be5bb44a9cd0ab35cc0b476b78b15d2c103
ST John <st--@users.noreply.github.com>
2022-03-16 17:06:19 +0200

    Revert "run test_approximation_predictions in SVA" didn't think; this won't work
    This reverts commit 37a541e740a82f5e29200186279300023d301d78.

--- test/SparseVariationalApproximationModule.jl
+++ test/SparseVariationalApproximationModule.jl
@@ -31,8 +31,6 @@
             AbstractGPs.TestUtils.test_internal_abstractgps_interface(rng, f_approx_post_Centered, a, b)
         end
 
-        ApproximateGPs.TestUtils.test_approximation_predictions(approx_Centered)
-
         @testset "NonCentered" begin
 
             # Construct optimal approximate posterior.
@@ -50,8 +48,6 @@
             # Construct equivalent approximate posteriors.
             approx_non_Centered = SparseVariationalApproximation(NonCentered(), fz, q_ε)
             f_approx_post_non_Centered = posterior(approx_non_Centered)
-
-            ApproximateGPs.TestUtils.test_approximation_predictions(approx_non_Centered)
 
             @testset "AbstractGPs interface - NonCentered" begin
                 AbstractGPs.TestUtils.test_internal_abstractgps_interface(
