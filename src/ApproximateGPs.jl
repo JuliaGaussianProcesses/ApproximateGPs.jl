09044de4bb4a4de1aa3428943f67b13c0d2b9067
st-- <st--@users.noreply.github.com>
2022-04-02 14:50:32 +0300

    Update src/ApproximateGPs.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/ApproximateGPs.jl
+++ src/ApproximateGPs.jl
@@ -25,7 +25,7 @@ include("TestUtils.jl")
 
 import ChainRulesCore: ProjectTo, Tangent
 using PDMats: ScalMat
-ProjectTo(x::T) where T <: ScalMat = ProjectTo{T}(; dim=x.dim, value=ProjectTo(x.value))
+ProjectTo(x::T) where {T<:ScalMat} = ProjectTo{T}(; dim=x.dim, value=ProjectTo(x.value))
 (pr::ProjectTo{<:ScalMat})(dx::ScalMat) = ScalMat(pr.dim, pr.value(dx.value))
 (pr::ProjectTo{<:ScalMat})(dx::Tangent{<:ScalMat}) = ScalMat(pr.dim, pr.value(dx.value))
 
